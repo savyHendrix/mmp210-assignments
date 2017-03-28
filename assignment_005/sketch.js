7e219162081fa0807e471b7cc6d8bde324073a06
savyHendrix <SavyHendrix@dhcp-10-30-34-233.bmcc.cuny.edu>
2017-03-28 16:06:33 -0400

    assign 5

--- assignment_005/sketch.js
+++ assignment_005/sketch.js
@@ -1,3 +1,100 @@
+//function setup(){
+//	createCanvas(500,500);
+//}
+//
+//function draw(){
+//	background(227,110,86);
+//	
+//	
+//	var wSize=80; //window size
+//	var wGrid=wSize/8 //grid size
+//	
+//	var x1=99;
+//	var y1=99;
+//	stroke(237,235,216);
+//	strokeWeight(3);
+//	fill(59,67,84);
+//	rect(x1,y1,wSize+1,wSize+1);
+//	
+//	var x1a=100;
+//	var y1a=100;
+//	var x2a=100;
+//	var y2a=100;
+//	
+//	//descending left upward lines
+//	strokeWeight(1.5);
+//	//descending left side
+//	y1a+=wGrid;
+//	x2a+=wGrid;
+//	line(x1a,y1a,x2a,y2a);
+//	y1a+=2*wGrid;
+//	x2a+=2*wGrid;
+//	line(x1a,y1a,x2a,y2a);
+//	y1a+=2*wGrid;
+//	x2a+=2*wGrid;
+//	line(x1a,y1a,x2a,y2a);
+//	y1a+=2*wGrid;
+//	x2a+=2*wGrid;
+//	line(x1a,y1a,x2a,y2a);
+//	//crossing bottom
+//	y1a+=wGrid;
+//	x2a+=wGrid;
+//	x1a+=wGrid;
+//	y2a+=wGrid;
+//	line(x1a,y1a,x2a,y2a);
+//	x1a+=2*wGrid;
+//	y2a+=2*wGrid;
+//	line(x1a,y1a,x2a,y2a);
+//	x1a+=2*wGrid;
+//	y2a+=2*wGrid;
+//	line(x1a,y1a,x2a,y2a);
+//	x1a+=2*wGrid;
+//	y2a+=2*wGrid;
+//	line(x1a,y1a,x2a,y2a);
+//	
+//	//ascending left downward lines
+//	//ascending left side
+//	var x1b=100;
+//	var y1b=180;
+//	var x2b=100;
+//	var y2b=180;
+//	y1b-=wGrid;
+//	x2b+=wGrid;
+//	line(x1b,y1b,x2b,y2b);
+//	y1b-=2*wGrid;
+//	x2b+=2*wGrid;
+//	line(x1b,y1b,x2b,y2b);
+//	y1b-=2*wGrid;
+//	x2b+=2*wGrid;
+//	line(x1b,y1b,x2b,y2b);
+//	y1b-=2*wGrid;
+//	x2b+=2*wGrid;
+//	line(x1b,y1b,x2b,y2b);
+//	//crossing top
+//	y1b-=wGrid;
+//	x2b+=wGrid;
+//	x1b+=wGrid;
+//	y2b-=wGrid;
+//	line(x1b,y1b,x2b,y2b);
+//	x1b+=2*wGrid;
+//	y2b-=2*wGrid;
+//	line(x1b,y1b,x2b,y2b);
+//	x1b+=2*wGrid;
+//	y2b-=2*wGrid;
+//	line(x1b,y1b,x2b,y2b);
+//	x1b+=2*wGrid;
+//	y2b-=2*wGrid;
+//	line(x1b,y1b,x2b,y2b);
+//	
+//	//inner square
+//	quad(x1+wGrid+1,y1+4*wGrid+1,x1+4*wGrid+1,y1+wGrid+1,x1+7*wGrid+1,y1+4*wGrid+1,x1+4*wGrid+1,y1+7*wGrid+1);
+//	line(x1+2*wGrid+1,y1+3*wGrid+1,x1+2*wGrid+1,y1+5*wGrid+1);
+//	line(x1+6*wGrid,y1+3*wGrid+1,x1+6*wGrid+1,y1+5*wGrid+1);
+//	line(x1+3*wGrid+1,y1+2*wGrid+1,x1+5*wGrid+1,y1+2*wGrid+1);
+//	line(x1+3*wGrid+1,y1+6*wGrid+1,x1+5*wGrid+1,y1+6*wGrid+1);
+//	
+//}
+
  //windows
 function setup() { 
    createCanvas(640, 480);
@@ -22,12 +119,9 @@ function draw() {
            rect(x, y, windowWidth/4, windowHeight/8);
            rect(x, y + windowHeight/8, windowWidth/4, windowHeight/8);
            rect(x + windowWidth/4, y + windowHeight/8, windowWidth/4, windowHeight/8);
-           rect(x + windowWidth/4, y + windowHeight/8 * 2, windowWidth/4, windowHeight/8);
-           
-           
+           rect(x + windowWidth/4, y + windowHeight/8 * 2, windowWidth/4, windowHeight/8);          
         }
-    }
-        
+    }      
 }
 
 // grid 
