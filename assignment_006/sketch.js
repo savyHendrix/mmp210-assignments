f3a015e23a419d287c53b25406330c1d46c133dd
savyHendrix <SavyHendrix@Savannahs-MBP.home>
2017-03-28 20:50:10 -0400

    assignment 006 finished

--- assignment_006/sketch.js
+++ assignment_006/sketch.js
@@ -1,9 +1,8 @@
-// transform animation
-
 var r = 0;
 var x = 0;
 var y = 0;
-//var n = 10;
+var n = 10;
+
 
 function setup() { 
     createCanvas(640, 480);
@@ -13,33 +12,47 @@ function setup() {
 
 function draw() {
     background(0);
-    
-    
-    push();
-    rotate(r);
-    r++;
-    fill(190,50,250);
-    ellipse(100,0,20);
-    pop();
-    pop();
+    r+=-0.05;
     
     translate(width/2, height/2);
     fill(255);
+	
+		push();
+	var g = random(0,255); //green
+    rotate(-r);
+    fill(190,g,250);
+    ellipse(280,0,20);
+    pop();
+		
+		push();
     rotate(r);
-//    r+=0.05;
+		if(n < height){
+			n+=frameCount;
+			ellipse(0,0,n);
+		}
+		else{
+			ellipse(0,0,n);
+		}
     
-    ellipse(0,0,20);
-    
-    
-//    n+=0.1;
-    for (var i = 0; i < 30; i++) {
-        for (var h = 0; h < width/2; h++){
+    for (var i = 0; i < 20; i++) {
+        for (var h = 0; h < width; h++){
             ellipse(0+h, 0, 5);
             rotate(PI/50);
         }
     }
+		pop();
     
-    
+		
+    if(n > height){
+			var a = random(0,255);
+			var b = random(0,255);
+			var c = random(0,255);
+			fill(a,b,c);
+			textSize(100);
+			textAlign(CENTER,CENTER);
+			textStyle(BOLD);
+			text("Hi.",x,y);
+		}
 
 }
 
