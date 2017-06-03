4def17d14f7f939bf9ad0d670c25a635e6caea98
savyHendrix <SavyHendrix@Savannahs-MacBook-Pro.local>
2017-06-03 02:19:16 -0400

    assign 4 update

--- assignment_004/sketch.js
+++ assignment_004/sketch.js
@@ -1,9 +1,17 @@
 var ellipseWidth = 0;
+
 function setup(){
     createCanvas(displayWidth,500);
-//    background('rgb(110,150,200)');
 }
+
 function draw(){
+	background(255);
+	
+	fill('rgb(250,140,190)');
+	noStroke();
+	rect(0, 0, width, height/2);
+	fill('rgb(110,150,200)');
+	rect(0, height/2, width, height/2);
 	
 	frameRate(10);
 	
@@ -23,7 +31,7 @@ function draw(){
 		var dark = color("rgb(19,106,193)");
 	}
 	else{
-		var light = color("rgb(255,155,255)");
+		var light = color("rgb(255,175,255)");
 		var dark = color("rgb(235,12,235)");
 	}
 //	stable ellipses
@@ -46,10 +54,12 @@ function draw(){
 	stroke(dark);
 		ellipse(mouseX, mouseY, ellipseWidth);
 }
-function mouseMoved(){
-	frameRate(60);
-	clear();
-}
+
+
+//function mouseMoved(){
+//	frameRate(60);
+//	clear();
+//}
 //function mouseClicked(){
 //		if(light == color("rgb(135,196,255)")){
 //			light = color("black");
--- final/sketch.js
+++ final/sketch.js
@@ -1,76 +1,85 @@
-var trees;
+var vanity;
+var vanityBottom;
+var mummy;
+var margin = 100;
+var vanityW;
+var newX = 570, newY = 200;
+var paletteW = 100;
+var paletteH = 40;
+var shadowPickUp;
 
 function preload(){
-    trees = loadImage("trees.jpg");
+    vanity = loadImage("gothicVanity_v2.png");
+    vanityBottom = loadImage("gothicVanityBottom.png");
+	mummy = loadImage("wrappedMummy_v1.png");
 }
 
-function setup() {
-    createCanvas(900, 700);
+function setup(){
+    createCanvas(800, 500);
     noStroke();
 //    canvas.parent('container'); add with #container in css
+	
 }
 
-function draw() {
-    background(0,0,0);
-    image(trees, 0, 0, width, height*3/4);
-    
-    //clearing
-    noStroke();
-    fill('green');
-    ellipse(width/2, height, width*1.5, height*3/4);
-    
-    //firesite
-        //fireRing
-        push();
-        stroke('black');
-        fill('gray');
-        translate(width/2, 600);
-        for(var i = 0; i < 20; i++){
-            ellipse(0, 80, 20);
-            rotate(PI/10);
-        }
-        pop();
-    
-        //wood
-        
-    
-    //match
-    push();
-    translate(width*4/5, 600);
-    stroke('black');
-    fill('tan');
-    rect(0, 0, 50, 4);
-    fill('red');
-    ellipse(0, 2, 12, 10);
-    pop();
-    
-    //rock
-    push();
-    translate(width/8, 600);
-    stroke('black');
-    fill('gray');
-    beginShape();
-        vertex(0, 0);
-        vertex(-4, -5);
-        vertex(-3, -10);
-        vertex(1, -13);
-        vertex(1, -20);
-        vertex(15, -30);
-        vertex(25, -31);
-        vertex(28, -25);
-        vertex(42, -29);
-        vertex(52, -20);
-        vertex(50, -12);
-        vertex(53, -10);
-        vertex(58, -4);
-        vertex(57, 0);
-    endShape(CLOSE);
-    pop();
-    
-    //fireAnimation
-    
-    
-    //wood scattered, must be gathered
-    
-    //matchbox ->click-> the box is all soggy ->click-> crap! only one left!
+function draw(){
+	background(40, 20, 20);
+	
+	vanityW = width-margin*2; //width of vanity
+	
+	push();
+	translate(margin, 0);
+	
+	
+    image(vanity, 0, 0, vanityW, height);
+    image(vanityBottom, -40, height*2/3, vanityW+80, height*3/5);
+    image(vanityBottom, -80, height*7/8, vanityW+160, height);
+	pop();
+	
+	image(mummy, width/2-280/2, height-459, 280, 459);
+
+
+// wig
+	ellipse(200, 170, 90, 130);
+//nail polish
+	ellipse(120, height*7/8, 30, 30);
+//powder
+	ellipse(margin+vanityW-20, height*2/3-15, 50, 50);
+//blush
+	ellipse(margin+vanityW-100, height*2/3, 90, 20);
+//eyeshadow palette
+	push();
+	translate(newX, newY);
+	
+	
+	var shadowW = paletteW/5;
+	
+	
+	fill(150);
+	rect(0, 0, paletteW, paletteH);
+	
+//eyeshadows
+	for(var x = shadowW; x < paletteW; x+=shadowW*1.5){
+		stroke(255);
+		strokeWeight(0.5);
+		var r = 0, g = 0, b = 0;
+		r += x;
+		fill(r, g, b);
+		ellipse(x, paletteH/2, shadowW);
+	}
+	shadowBrush(paletteW/2, paletteH);
+	pop();
 }
+
+function shadowBrush(x, y){
+	var w = 30;
+	var h = 5;
+	
+	noStroke();
+	fill(200);
+	rect(x, y, w, h);
+	fill(255);
+	ellipse(x, y+h/2, w/2, 2*h);
+	
+}
+
+//lash function w open and clsoed container
